e8b1d31fcd98f03fcbc14935f302af13afb0650e
Sachinandan <sachinandanp5@gmail.com>
2024-07-28 23:34:45 +0530

    footer set

--- src/compnents/SideBar.jsx
+++ src/compnents/SideBar.jsx
@@ -1,11 +1,18 @@
-
-
 export default function SideBar() {
-    return (
-        <>
-        <div className="border-4 border-sky-500">
+  return (
+    <>
+      <div className="border-2 border-sky-500">
         SIDE BAR
-        </div>
-        </>
-    );
-  }
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+      </div>
+    </>
+  );
+}
